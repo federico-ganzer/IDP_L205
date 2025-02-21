9cc241dfa80551b7e74507a8b8517c4029967b01
Federico Ganzer <fg442@cam.ac.uk>
2025-02-21 10:02:08 +0000

    fixed graph labels

--- pathfinder.py
+++ pathfinder.py
@@ -8,17 +8,17 @@ Twin of each path is also defined [end, start, -1*path[list][:-1], length, nturn
  # 'start_node_id': {connected_node_id: distance}
 graph = { 'START': [('3', 1.0)],
 
-            '1': [('dp2', 1.0), ('5', 2.0), ('2', 1.0)],
+            '1': [('DP2', 1.0), ('5', 2.0), ('2', 1.0)],
 
             '2': [('A', 1.0), ('3', 1.0), ('1', 1.0)],
 
-            '3': [('start', 1.0), ('2', 1.0), ('4', 2.0)],
+            '3': [('START', 1.0), ('2', 1.0), ('4', 2.0)],
 
             'DP1': [('4', 1.0)],
 
             'DP2': [('1', 1.0)],
 
-            '4': [('dp1', 1.0), ('3', 2.0), ('8', 2.0)],
+            '4': [('DP1', 1.0), ('3', 2.0), ('8', 2.0)],
 
             '5': [('1', 2.0), ('6', 2.0), ('10', 4.0)],
 
@@ -44,6 +44,9 @@ graph = { 'START': [('3', 1.0)],
         }
 
 def dijkstra(adj_list, start, end):
+    '''
+    Finds shortest path from start to end using Dijkstra's algorithm.
+    '''
     pq = [(0, start)]
     distances = {start: 0}
     pred = {start: None}
@@ -59,7 +62,7 @@ def dijkstra(adj_list, start, end):
             return path[::-1]
         
         for neighbor, distance in adj_list.get(node, []):
-            new_distance = current_distance + distance
+            new_distance = current_distance + distance # modification can be made here to include turns
             if neighbor not in distances or new_distance < distances[neighbor]:
                 distances[neighbor] = new_distance
                 heapq.heappush(pq, (new_distance, neighbor))
@@ -68,10 +71,8 @@ def dijkstra(adj_list, start, end):
     return None
 
 #test
-#print(dijkstra(graph, 'START', 'C'))
+#print(dijkstra(graph, 'C', 'DP1'))
 
 def find_route(start, end):
-    '''
-    Find the shortest path from start to end using BFS
-    '''
+    
     return

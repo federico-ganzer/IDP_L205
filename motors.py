a8a42fea95d2009b6211ff25d9cb5c4aa11806d5
Federico Ganzer <fg442@cam.ac.uk>
2025-03-19 09:57:57 +0000

    add rspeed for reverse to motors class

--- motors.py
+++ motors.py
@@ -9,18 +9,21 @@ class Motor():
         self.pwm.freq(1000) # set max frequency
         self.pwm.duty_u16(0) # set duty cycle
         self.speed = 0
+        self.rspeed = 0
 
     def forward(self, speed):
         if speed == self.speed:
             self.dir.value(0)
         else:
+            self.speed = speed
             self.dir.value(0)
             self.pwm.duty_u16(int(65535*speed/100))
 
     def reverse(self, speed):
-        if speed == self.speed:
+        if speed == self.rspeed:
             self.dir.value(1)
         else:
+            self.rspeed = speed
             self.dir.value(1)
             self.pwm.duty_u16(int(65535*speed/100))
 
